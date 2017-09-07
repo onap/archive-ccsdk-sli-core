49754a80d52f9dc5e63d53c299fcf453c90ceffd
shashikanth <shashikanth.vh@huawei.com>
2017-09-07 18:42:34 +0530

    Fix Blocker/Critical sonar issues
    Fix Blocker/Critical sonar issues in vnfapi module
    https://sonar.onap.org/component_issues?id=org.openecomp.sdnc.northbound%3Asdnc-northbound#resolved=false|severities=BLOCKER%2CCRITICAL
    Issue-Id: SDNC-65
    
    Change-Id: I60e1f50b08cfd990a7e2d3c65c8237b230f2289f
    Signed-off-by: shashikanth.vh <shashikanth.vh@huawei.com>

--- dblib/provider/src/main/java/org/onap/ccsdk/sli/core/dblib/DBResourceManager.java
+++ dblib/provider/src/main/java/org/onap/ccsdk/sli/core/dblib/DBResourceManager.java
@@ -20,6 +20,19 @@
 
 package org.onap.ccsdk.sli.core.dblib;
 
+import com.mysql.jdbc.exceptions.jdbc4.MySQLNonTransientConnectionException;
+import org.apache.tomcat.jdbc.pool.PoolExhaustedException;
+import org.onap.ccsdk.sli.core.dblib.config.DbConfigPool;
+import org.onap.ccsdk.sli.core.dblib.factory.AbstractDBResourceManagerFactory;
+import org.onap.ccsdk.sli.core.dblib.factory.AbstractResourceManagerFactory;
+import org.onap.ccsdk.sli.core.dblib.factory.DBConfigFactory;
+import org.onap.ccsdk.sli.core.dblib.pm.PollingWorker;
+import org.onap.ccsdk.sli.core.dblib.pm.SQLExecutionMonitor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.sql.DataSource;
+import javax.sql.rowset.CachedRowSet;
 import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.SQLDataException;
@@ -38,25 +51,8 @@ import java.util.PriorityQueue;
 import java.util.Properties;
 import java.util.Queue;
 import java.util.Set;
-import java.util.Timer;
 import java.util.TimerTask;
-import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.atomic.AtomicBoolean;
-
-import javax.sql.DataSource;
-import javax.sql.rowset.CachedRowSet;
-
-import org.apache.tomcat.jdbc.pool.PoolExhaustedException;
-import org.onap.ccsdk.sli.core.dblib.config.DbConfigPool;
-import org.onap.ccsdk.sli.core.dblib.factory.AbstractDBResourceManagerFactory;
-import org.onap.ccsdk.sli.core.dblib.factory.AbstractResourceManagerFactory;
-import org.onap.ccsdk.sli.core.dblib.factory.DBConfigFactory;
-import org.onap.ccsdk.sli.core.dblib.pm.PollingWorker;
-import org.onap.ccsdk.sli.core.dblib.pm.SQLExecutionMonitor;
-
-import com.mysql.jdbc.exceptions.jdbc4.MySQLNonTransientConnectionException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 
 /**
@@ -86,7 +82,7 @@ public class DBResourceManager implements DataSource, DataAccessor, DBResourceOb
 				if(!right.isSlave())
 					return 1;
 
-			} catch (Throwable e) {
+			} catch (Exception e) {
 				LOGGER.warn("", e);
 			}
 			return 0;
@@ -444,9 +440,7 @@ public class DBResourceManager implements DataSource, DataAccessor, DBResourceOb
 				return master;
 			}
 		}
-		if(master == null) {
-			LOGGER.warn("MASTER not found.");
-		}
+		LOGGER.warn("MASTER not found.");
 		return master;
 	}
 
