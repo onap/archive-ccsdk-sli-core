cb9adb546da5590d944ba907d4145f4a7a742933
Rich Tabedzki <richard.tabedzki@att.com>
2017-09-14 02:54:43 +0000

    [CCSDK-92] summary
    Changes made:
    1. removed hardcoded JDBC driver's name and made it a property.
    2. Optimized connection pool initialization
    3. Removed unused factories.
    4. In DblibJREFileResolver checked for null value of bundle variable.
    
    Change-Id: I86a3a1355ba453a4094c5714be4530915e618947
    Signed-off-by: Rich Tabedzki <richard.tabedzki@att.com>

--- dblib/provider/src/main/java/org/onap/ccsdk/sli/core/dblib/config/BaseDBConfiguration.java
+++ dblib/provider/src/main/java/org/onap/ccsdk/sli/core/dblib/config/BaseDBConfiguration.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -22,88 +22,231 @@ package org.onap.ccsdk.sli.core.dblib.config;
 
 import java.util.Properties;
 
+/**
+ * Base class responsible for parsing business logic for database configuration
+ * from given <code>Properties</code>.
+ */
 public abstract class BaseDBConfiguration {
-	public static final String DATABASE_TYPE	= "org.onap.ccsdk.sli.dbtype";
-	public static final String DATABASE_URL		= "org.onap.ccsdk.sli.jdbc.url";
-	public static final String DATABASE_NAME	= "org.onap.ccsdk.sli.jdbc.database";
-	public static final String DRIVER_NAME		= "org.onap.ccsdk.sli.jdbc.driver";
-	public static final String CONNECTION_NAME	= "org.onap.ccsdk.sli.jdbc.connection.name";
-	public static final String DATABASE_USER 	= "org.onap.ccsdk.sli.jdbc.user";
-	public static final String DATABASE_PSSWD	= "org.onap.ccsdk.sli.jdbc.password";
-	public static final String CONNECTION_TIMEOUT="org.onap.ccsdk.sli.jdbc.connection.timeout";
-	public static final String REQUEST_TIMEOUT	= "org.onap.ccsdk.sli.jdbc.request.timeout";
-	public static final String MIN_LIMIT		= "org.onap.ccsdk.sli.jdbc.limit.min";
-	public static final String MAX_LIMIT		= "org.onap.ccsdk.sli.jdbc.limit.max";
-	public static final String INIT_LIMIT		= "org.onap.ccsdk.sli.jdbc.limit.init";
-	public static final String DATABASE_HOSTS   = "org.onap.ccsdk.sli.jdbc.hosts";
 
+	/**
+	 * Property key within a properties configuration File for db type
+	 */
+	public static final String DATABASE_TYPE = "org.onap.ccsdk.sli.dbtype";
 
-	protected final Properties props;
+	/**
+	 * Property key with a properties configuration File for db url
+	 */
+	public static final String DATABASE_URL = "org.onap.ccsdk.sli.jdbc.url";
 
-	public BaseDBConfiguration(Properties properties) {
-		this.props = properties;
+	/**
+	 * Property key with a properties configuration File for database name
+	 */
+	public static final String DATABASE_NAME = "org.onap.ccsdk.sli.jdbc.database";
+
+	/**
+	 * Property key with a properties configuration File for jdbc driver
+	 */
+	public static final String DRIVER_NAME = "org.onap.ccsdk.sli.jdbc.driver";
+
+	/**
+	 * Property key with a properties configuration File for db database
+	 * connection name
+	 */
+	public static final String CONNECTION_NAME = "org.onap.ccsdk.sli.jdbc.connection.name";
+
+	/**
+	 * Property key with a properties configuration File for database user
+	 */
+	public static final String DATABASE_USER = "org.onap.ccsdk.sli.jdbc.user";
+
+	/**
+	 * Property key with a properties configuration File for database password
+	 * for associated with <code>org.onap.ccsdk.sli.jdbc.user</code>.
+	 */
+	public static final String DATABASE_PSSWD = "org.onap.ccsdk.sli.jdbc.password";
+
+	/**
+	 * Property key with a properties configuration File for database connection
+	 * timeout
+	 */
+	public static final String CONNECTION_TIMEOUT = "org.onap.ccsdk.sli.jdbc.connection.timeout";
+
+	/**
+	 * Property key with a properties configuration File for database request
+	 * timeout
+	 */
+	public static final String REQUEST_TIMEOUT = "org.onap.ccsdk.sli.jdbc.request.timeout";
+
+	/**
+	 * Property key with a properties configuration File for database minimum
+	 * limit
+	 */
+	public static final String MIN_LIMIT = "org.onap.ccsdk.sli.jdbc.limit.min";
+
+	/**
+	 * Property key with a properties configuration File for database maximum
+	 * limit
+	 */
+	public static final String MAX_LIMIT = "org.onap.ccsdk.sli.jdbc.limit.max";
+
+	/**
+	 * Property key with a properties configuration File for database initial
+	 * limit
+	 */
+	public static final String INIT_LIMIT = "org.onap.ccsdk.sli.jdbc.limit.init";
+
+	/**
+	 * Property key with a properties configuration File for database hosts
+	 */
+	public static final String DATABASE_HOSTS = "org.onap.ccsdk.sli.jdbc.hosts";
+
+	/**
+	 * default value when the connection timeout is not present or cannot be
+	 * parsed
+	 */
+	private static final String DEFAULT_CONNECTION_TIMEOUT = "-1";
+
+	/**
+	 * default value when the request timeout is not present or cannot be parsed
+	 */
+	private static final String DEFAULT_REQUEST_TIMEOUT = "-1";
+
+	/**
+	 * A set of properties with database configuration information.
+	 */
+	protected final Properties properties;
+
+	/**
+	 * Builds a configuration based on given properties
+	 *
+	 * @param properties
+	 *            properties represented by the public constant keys defined by
+	 *            this class
+	 */
+	public BaseDBConfiguration(final Properties properties) {
+		this.properties = properties;
 	}
 
+	/**
+	 * Extracts the connection timeout.
+	 *
+	 * @return the connection timeout, or
+	 *         <code>DEFAULT_CONNECTION_TIMEOUT</code> if not present
+	 */
 	public int getConnTimeout() {
 		try {
-			String value = props.getProperty(CONNECTION_TIMEOUT);
+			String value = properties.getProperty(CONNECTION_TIMEOUT, DEFAULT_CONNECTION_TIMEOUT);
 			return Integer.parseInt(value);
-		} catch(Exception exc) {
-			return -1;
+		} catch (Exception exc) {
+			return Integer.parseInt(DEFAULT_CONNECTION_TIMEOUT);
 		}
 	}
 
+	/**
+	 * Extracts the request timeout.
+	 *
+	 * @return the request timeout, or <code>DEFAULT_REQUEST_TIMEOUT</code> if
+	 *         not present
+	 */
 	public int getRequestTimeout() {
 		try {
-			String value = props.getProperty(REQUEST_TIMEOUT);
-			if(value == null)
-				return -1;
+			String value = properties.getProperty(REQUEST_TIMEOUT, DEFAULT_REQUEST_TIMEOUT);
 			return Integer.parseInt(value);
-		} catch(Exception exc) {
-			return -1;
+		} catch (Exception exc) {
+			return Integer.parseInt(DEFAULT_REQUEST_TIMEOUT);
 		}
 	}
 
+	/**
+	 * Extracts the db connection name.
+	 *
+	 * @return the db connection name, or <code>null</code> if not present
+	 */
 	public String getDbConnectionName() {
-		return props.getProperty(CONNECTION_NAME);
+		return properties.getProperty(CONNECTION_NAME);
 	}
 
+	/**
+	 * Extracts the db name.
+	 *
+	 * @return the db name, or <code>null</code> if not present
+	 */
 	public String getDatabaseName() {
-		return props.getProperty(DATABASE_NAME);
+		return properties.getProperty(DATABASE_NAME);
 	}
 
+	/**
+	 * Extracts the jdbc driver's name.
+	 *
+	 * @return the jdbc name, or <code>com.mysql.jdbc.Driver</code> if not present
+	 */
 	public String getDriverName() {
-		return props.getProperty(DRIVER_NAME, "com.mysql.jdbc.Driver");
+		return properties.getProperty(DRIVER_NAME, "com.mysql.jdbc.Driver");
 	}
 
+	/**
+	 * Extracts the db user id.
+	 *
+	 * @return the db user id, or <code>null</code> if not present
+	 */
 	public String getDbUserId() {
-		return props.getProperty(DATABASE_USER);
+		return properties.getProperty(DATABASE_USER);
 	}
 
+	/**
+	 * Extracts the db password.
+	 *
+	 * @return the db password, or <code>null</code> if not present
+	 */
 	public String getDbPasswd() {
-		return props.getProperty(DATABASE_PSSWD);
+		return properties.getProperty(DATABASE_PSSWD);
 	}
 
-	public int getDbMinLimit() {
-		String value = props.getProperty(MIN_LIMIT);
+	/**
+	 * Extracts the db min limit.
+	 *
+	 * @return the db min limit
+	 * @throws NumberFormatException
+	 *             if the property is not specified, or cannot be parsed as an
+	 *             <code>Integer</code>.
+	 */
+	public int getDbMinLimit() throws NumberFormatException {
+		String value = properties.getProperty(MIN_LIMIT);
 		return Integer.parseInt(value);
 	}
 
-	public int getDbMaxLimit() {
-		String value = props.getProperty(MAX_LIMIT);
+	/**
+	 * Extracts the db max limit.
+	 *
+	 * @return the db max limit
+	 * @throws NumberFormatException
+	 *             if the property is not specified, or cannot be parsed as an
+	 *             <code>Integer</code>.
+	 */
+	public int getDbMaxLimit() throws NumberFormatException {
+		String value = properties.getProperty(MAX_LIMIT);
 		return Integer.parseInt(value);
 	}
 
-	public int getDbInitialLimit() {
-		String value = props.getProperty(INIT_LIMIT);
+	/**
+	 * Extracts the db initial limit.
+	 *
+	 * @return the db initial limit
+	 * @throws NumberFormatException
+	 *             if the property is not specified, or cannot be parsed as an
+	 *             <code>Integer</code>.
+	 */
+	public int getDbInitialLimit() throws NumberFormatException {
+		String value = properties.getProperty(INIT_LIMIT);
 		return Integer.parseInt(value);
 	}
 
+	/**
+	 * Extracts the db url.
+	 *
+	 * @return the db url, or <code>null</code> if not present
+	 */
 	public String getDbUrl() {
-		return props.getProperty(DATABASE_URL);
-	}
-
-	public String getServerGroup() {
-		return null;
+		return properties.getProperty(DATABASE_URL);
 	}
 }
