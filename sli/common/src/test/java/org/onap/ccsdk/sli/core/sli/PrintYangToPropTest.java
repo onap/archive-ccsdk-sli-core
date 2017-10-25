90ffdb47e259dcbad0f76cad385c0f0b2f2a5428
Dan Timoney <dtimoney@att.com>
2017-10-25 14:08:31 -0400

    Add unit test case for PrintYangToProp
    Add junit testing for PrintYangToProp
    
    Change-Id: I8c377bb51c812ea9ead32d8e9d5ef871157162a8
    Issue-ID: CCSDK-129
    Signed-off-by: Dan Timoney <dtimoney@att.com>

---
+++ sli/common/src/test/java/org/onap/ccsdk/sli/core/sli/PrintYangToPropTest.java
@@ -0,0 +1,60 @@
+/**
+ *
+ */
+package org.onap.ccsdk.sli.core.sli;
+
+import static org.junit.Assert.*;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Properties;
+
+import org.junit.Test;
+import org.opendaylight.yang.gen.v1.org.onap.ccsdk.sli.core.sliapi.rev161110.ExecuteGraphInput.Mode;
+import org.opendaylight.yang.gen.v1.org.onap.ccsdk.sli.core.sliapi.rev161110.ExecuteGraphInputBuilder;
+import org.opendaylight.yang.gen.v1.org.onap.ccsdk.sli.core.sliapi.rev161110.execute.graph.input.SliParameter;
+import org.opendaylight.yang.gen.v1.org.onap.ccsdk.sli.core.sliapi.rev161110.execute.graph.input.SliParameterBuilder;
+
+/**
+ * @author dt5972
+ *
+ */
+public class PrintYangToPropTest {
+
+	@Test
+	public void test() {
+
+		Properties props = new Properties();
+
+		// Set up a builder with data
+		ExecuteGraphInputBuilder egBuilder = new ExecuteGraphInputBuilder();
+		egBuilder.setMode(Mode.Sync);
+		egBuilder.setModuleName("my-module");
+		egBuilder.setRpcName("my-rpc");
+
+		List<SliParameter> pList = new LinkedList<>();
+
+		SliParameterBuilder pBuilder = new SliParameterBuilder();
+		pBuilder.setParameterName("string-param");
+		pBuilder.setStringValue("hi");
+		pList.add(pBuilder.build());
+		pBuilder.setParameterName("int-param");
+		pBuilder.setIntValue(1);
+		pBuilder.setStringValue(null);
+		pList.add(pBuilder.build());
+		pBuilder.setParameterName("bool-param");
+		pBuilder.setIntValue(null);
+		pBuilder.setBooleanValue(true);
+		pList.add(pBuilder.build());
+
+		egBuilder.setSliParameter(pList);
+
+
+		// Generate properties
+		props = PrintYangToProp.toProperties(props, egBuilder);
+
+
+
+	}
+
+}
