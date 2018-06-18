784e3ef65f03017ea82055dcedab73a5b04e4972
Timoney, Dan (dt5972) <dt5972@att.com>
2018-06-18 17:11:56 -0400

    Update sli/core/utils version
    Update sli/core/utils features versions to ODL Oxygen
    
    Change-Id: Icef6bd776bf489b3991dc2406bccfea4104b1299
    Issue-ID: CCSDK-280
    Signed-off-by: Timoney, Dan (dt5972) <dt5972@att.com>

--- sliPluginUtils/installer/src/main/resources/scripts/install-feature.sh
+++ sliPluginUtils/installer/src/main/resources/scripts/install-feature.sh
@@ -29,7 +29,7 @@ REPOZIP=${INSTALLERDIR}/${features.boot}-${project.version}.zip
 
 if [ -f ${REPOZIP} ]
 then
-	unzip -d ${ODL_HOME} ${REPOZIP}
+	unzip -nd ${ODL_HOME} ${REPOZIP}
 else
 	echo "ERROR : repo zip ($REPOZIP) not found"
 	exit 1
