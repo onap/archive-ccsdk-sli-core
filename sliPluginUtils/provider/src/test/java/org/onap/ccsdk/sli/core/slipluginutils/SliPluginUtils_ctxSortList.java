3aa65729d5ed0170201748dcc54c413611fa5cbb
Sandeep J <sandeejh@in.ibm.com>
2018-09-26 02:59:01 +0530

    added test case to SliPluginUtils_ctxSortList
    to increase code coverage
    
    Issue-ID: CCSDK-595
    Change-Id: I0cfaac418d169542b92b6f60eb1b8c32a1a95f37
    Signed-off-by: Sandeep J <sandeejh@in.ibm.com>

--- sliPluginUtils/provider/src/test/java/org/onap/ccsdk/sli/core/slipluginutils/SliPluginUtils_ctxSortList.java
+++ sliPluginUtils/provider/src/test/java/org/onap/ccsdk/sli/core/slipluginutils/SliPluginUtils_ctxSortList.java
@@ -3,7 +3,9 @@
  * ONAP : CCSDK
  * ================================================================================
  * Copyright (C) 2017 AT&T Intellectual Property. All rights
- * 						reserved.
+ *                         reserved.
+ * ================================================================================
+ * Modifications Copyright (C) 2018 IBM.
  * ================================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -21,6 +23,7 @@
 
 package org.onap.ccsdk.sli.core.slipluginutils;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import java.util.HashMap;
@@ -35,62 +38,104 @@ import org.slf4j.LoggerFactory;
 
 @SuppressWarnings("unused")
 public class SliPluginUtils_ctxSortList {
-	private static final Logger LOG = LoggerFactory.getLogger(SliPluginUtils_ctxSortList.class);
-	SliPluginUtils utils = new SliPluginUtils();
-	SvcLogicContext ctx;
-	HashMap<String,String> parameters;
-	Random rand = new Random();
+    private static final Logger LOG = LoggerFactory.getLogger(SliPluginUtils_ctxSortList.class);
+    SliPluginUtils utils = new SliPluginUtils();
+    SvcLogicContext ctx;
+    HashMap<String,String> parameters;
+    Random rand = new Random();
 
-	@Before
-	public void setUp() throws Exception {
-		this.ctx = new SvcLogicContext();
-		this.parameters = new HashMap<String,String>();
-	}
+    @Before
+    public void setUp() throws Exception {
+        this.ctx = new SvcLogicContext();
+        this.parameters = new HashMap<String,String>();
+    }
 
-	@Test
-	public final void list_of_containers() throws SvcLogicException {
-		this.parameters.put("list", "input.list");
-		this.parameters.put("sort-fields", "sort-key");
-		this.parameters.put("delimiter",",");
+    @Test
+    public final void list_of_containers() throws SvcLogicException {
+        this.parameters.put("list", "input.list");
+        this.parameters.put("sort-fields", "sort-key");
+        this.parameters.put("delimiter",",");
 
-		ctx.setAttribute("input.list_length", "10");
-		for( int i = 0; i < 10; i++ ) {
-			this.ctx.setAttribute("input.list[" + i + "].sort-key", Integer.toString( rand.nextInt(10) ));
-			this.ctx.setAttribute("input.list[" + i + "].value", Integer.toString( rand.nextInt(10) ));
-		}
+        ctx.setAttribute("input.list_length", "10");
+        for( int i = 0; i < 10; i++ ) {
+            this.ctx.setAttribute("input.list[" + i + "].sort-key", Integer.toString( rand.nextInt(10) ));
+            this.ctx.setAttribute("input.list[" + i + "].value", Integer.toString( rand.nextInt(10) ));
+        }
 
-		LOG.trace("BEFORE SORT:");
-		SliPluginUtils.logContextMemory(ctx, LOG, SliPluginUtils.LogLevel.TRACE);
+        LOG.trace("BEFORE SORT:");
+        SliPluginUtils.logContextMemory(ctx, LOG, SliPluginUtils.LogLevel.TRACE);
 
-		utils.ctxSortList(this.parameters, this.ctx);
+        utils.ctxSortList(this.parameters, this.ctx);
 
-		LOG.trace("AFTER SORT:");
-		SliPluginUtils.logContextMemory(ctx, LOG, SliPluginUtils.LogLevel.TRACE);
+        LOG.trace("AFTER SORT:");
+        SliPluginUtils.logContextMemory(ctx, LOG, SliPluginUtils.LogLevel.TRACE);
 
-		for( int i = 0; i < 9; i++ ) {
-			assertTrue(this.ctx.getAttribute("input.list[" + i + "].sort-key").compareTo(this.ctx.getAttribute("input.list[" + (i+1) + "].sort-key")) < 1 );
-		}
-	}
+        for( int i = 0; i < 9; i++ ) {
+            assertTrue(this.ctx.getAttribute("input.list[" + i + "].sort-key").compareTo(this.ctx.getAttribute("input.list[" + (i+1) + "].sort-key")) < 1 );
+        }
+    }
 
-	@Test public final void list_of_elements() throws SvcLogicException {
-		this.parameters.put("list", "input.list");
-		this.parameters.put("delimiter",",");
+    @Test 
+    public final void list_of_elements() throws SvcLogicException {
+        this.parameters.put("list", "input.list");
+        this.parameters.put("delimiter",",");
 
-		this.ctx.setAttribute("input.list_length", "10");
-		for( int i = 0; i < 10; i++ ) {
-			this.ctx.setAttribute("input.list[" + i + ']', Integer.toString( rand.nextInt(10) ));
-		}
+        this.ctx.setAttribute("input.list_length", "10");
+        for( int i = 0; i < 10; i++ ) {
+            this.ctx.setAttribute("input.list[" + i + ']', Integer.toString( rand.nextInt(10) ));
+        }
 
-		LOG.trace("BEFORE SORT:");
-		SliPluginUtils.logContextMemory(ctx, LOG, SliPluginUtils.LogLevel.TRACE);
+        LOG.trace("BEFORE SORT:");
+        SliPluginUtils.logContextMemory(ctx, LOG, SliPluginUtils.LogLevel.TRACE);
 
-		utils.ctxSortList(this.parameters, this.ctx);
+        utils.ctxSortList(this.parameters, this.ctx);
 
-		LOG.trace("AFTER SORT:");
-		SliPluginUtils.logContextMemory(ctx, LOG, SliPluginUtils.LogLevel.TRACE);
+        LOG.trace("AFTER SORT:");
+        SliPluginUtils.logContextMemory(ctx, LOG, SliPluginUtils.LogLevel.TRACE);
 
-		for( int i = 0; i < 9; i++ ) {
-			assertTrue(this.ctx.getAttribute("input.list[" + i + ']').compareTo(this.ctx.getAttribute("input.list[" + (i+1) + ']')) < 1 );
-		}
-	}
+        for( int i = 0; i < 9; i++ ) {
+            assertTrue(this.ctx.getAttribute("input.list[" + i + ']').compareTo(this.ctx.getAttribute("input.list[" + (i+1) + ']')) < 1 );
+        }
+    }
+    
+    @Test
+    public void testGenerateUUID() throws SvcLogicException
+    {
+        SliPluginUtils utils= new SliPluginUtils();
+        this.parameters.put("ctx-destination", "testDestination");
+        utils.generateUUID(this.parameters, ctx);
+    }
+    
+    @Test
+    public void testSubstring() throws SvcLogicException 
+    {
+        SliPluginUtils utils= new SliPluginUtils();
+        this.parameters.put("string", "testString");
+        this.parameters.put("begin-index", "1");
+        this.parameters.put("result", "testResult");
+        this.parameters.put("end-index", "5");
+        utils.substring(this.parameters, ctx);
+        assertEquals("estS",ctx.getAttribute("testResult"));
+    }
+    
+    @Test
+    public void testSubstringForNullEndIndex() throws SvcLogicException 
+    {
+        SliPluginUtils utils= new SliPluginUtils();
+        this.parameters.put("string", "testString");
+        this.parameters.put("begin-index", "1");
+        this.parameters.put("result", "testResult");
+        utils.substring(this.parameters, ctx);
+        assertEquals("estString",ctx.getAttribute("testResult"));
+    }
+    
+    @Test
+    public void testCtxBulkCopy()
+    {
+        ctx.setAttribute("Mykey1", "MyValue1");
+        ctx.setAttribute("Mykey2", "MyValue2");
+        SliPluginUtils.ctxBulkCopy(ctx, "Mykey", "test.");
+        assertEquals("MyValue1",ctx.getAttribute("test.1"));
+        assertEquals("MyValue2",ctx.getAttribute("test.2"));
+    }
 }
