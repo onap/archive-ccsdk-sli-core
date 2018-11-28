f182003d772efe8f9d22876b1a5b7991f4e14afc
Brandon, Bruce (bb2697) <bb2697@att.com>
2018-11-28 22:10:56 +0000

    Add DG name to debug
    Add DG name to debug line giving node ID execution
    
    Change-Id: I95ef74982694ab53068339cce88ec3ea8ccf2258
    Issue-ID: SDNC-538
    Signed-off-by: Brandon, Bruce (bb2697) <bb2697@att.com>

--- sli/provider/src/main/java/org/onap/ccsdk/sli/core/sli/provider/SvcLogicServiceImpl.java
+++ sli/provider/src/main/java/org/onap/ccsdk/sli/core/sli/provider/SvcLogicServiceImpl.java
@@ -173,9 +173,9 @@ public class SvcLogicServiceImpl implements SvcLogicService {
         }
 
 		LOG.info("About to execute node # {} ({})", node.getNodeId(), node.getNodeType());
-		
+
         if (LOG.isDebugEnabled()) {
-            LOG.debug("Executing node {}", node.getNodeId());
+            LOG.debug("Executing node " + node.getNodeId() + " of " + node.getGraph().getRpc());
         }
 
         SvcLogicNodeExecutor executor = nodeExecutors.get(node.getNodeType());
