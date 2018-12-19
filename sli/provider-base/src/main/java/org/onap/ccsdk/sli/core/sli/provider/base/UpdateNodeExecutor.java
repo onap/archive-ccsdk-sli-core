06eec44b6c2a3456d3a417af0740a7085612b615
Sandeep J <sandeejh@in.ibm.com>
2018-12-19 12:44:43 +0530

    fixed sonar issue in UpdateNodeExecutor.java
    to increase code coverage
    
    Issue-ID: CCSDK-525
    Change-Id: I217ef3ac414ce2c57ba10e99c071ea91d4ae3904
    Signed-off-by: Sandeep J <sandeejh@in.ibm.com>

--- sli/provider-base/src/main/java/org/onap/ccsdk/sli/core/sli/provider/base/UpdateNodeExecutor.java
+++ sli/provider-base/src/main/java/org/onap/ccsdk/sli/core/sli/provider/base/UpdateNodeExecutor.java
@@ -5,6 +5,8 @@
  * Copyright (C) 2017 AT&T Intellectual Property. All rights
  * 						reserved.
  * ================================================================================
+ * Modifications Copyright (C) 2018 IBM.
+ * ================================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -47,7 +49,7 @@ public class UpdateNodeExecutor extends AbstractSvcLogicNodeExecutor {
         String pfx = SvcLogicExpressionResolver.evaluate(node.getAttribute("pfx"), node, ctx);
 
 
-        Map<String, String> parmMap = new HashMap<String, String>();
+        Map<String, String> parmMap = new HashMap<>();
 
         Set<Map.Entry<String, SvcLogicExpression>> parmSet = node.getParameterSet();
         boolean hasParms = false;
