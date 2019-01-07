3d109b51a789f781abd6d85b5f26c46469e0aa6e
Rich Tabedzki <richard.tabedzki@att.com>
2019-01-07 15:10:11 -0500

    fixed sonar issue in JdbcCachedDataSource
    Changes made:
    * updated cleanup code
    * added TerminatingConfiguration class
    
    Change-Id: Iee16116a5fc419065915b9f84488874dd192af7c
    Issue-ID: CCSDK-913
    Signed-off-by: Rich Tabedzki <richard.tabedzki@att.com>

--- dblib/provider/src/main/java/org/onap/ccsdk/sli/core/dblib/config/TerminatingConfiguration.java
+++ dblib/provider/src/main/java/org/onap/ccsdk/sli/core/dblib/config/TerminatingConfiguration.java
@@ -4,12 +4,14 @@
  * ================================================================================
  * Copyright (C) 2016 - 2017 ONAP
  * ================================================================================
+ * Modifications Copyright (C) 2018 IBM.
+ * ================================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * 
  *      http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -18,35 +20,14 @@
  * ============LICENSE_END=========================================================
  */
 
-package org.onap.ccsdk.sli.core.dblib;
+package org.onap.ccsdk.sli.core.dblib.config;
 
-import java.sql.SQLFeatureNotSupportedException;
-import java.util.logging.Logger;
-import javax.sql.DataSource;
-import org.onap.ccsdk.sli.core.dblib.config.BaseDBConfiguration;
-import org.onap.ccsdk.sli.core.dblib.pm.SQLExecutionMonitorObserver;
+import java.util.Properties;
 
+public class TerminatingConfiguration extends BaseDBConfiguration {
 
-public class TerminatingCachedDataSource extends CachedDataSource implements SQLExecutionMonitorObserver {
-
-    private static final int DEFAULT_AVAILABLE_CONNECTIONS = 0;
-
-    public TerminatingCachedDataSource(BaseDBConfiguration jdbcElem) throws DBConfigException {
-        super(jdbcElem);
+    public TerminatingConfiguration() {
+        super(new Properties());
     }
 
-    @Override
-    protected DataSource configure(BaseDBConfiguration jdbcElem) throws DBConfigException {
-        return null;
-    }
-
-    @Override
-    public Logger getParentLogger() throws SQLFeatureNotSupportedException {
-        return null;
-    }
-
-    @Override
-    protected int getAvailableConnections() {
-        return DEFAULT_AVAILABLE_CONNECTIONS;
-    }
 }
