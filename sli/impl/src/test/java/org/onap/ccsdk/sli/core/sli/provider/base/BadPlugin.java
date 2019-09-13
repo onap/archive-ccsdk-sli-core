c5abbe16ff43e5082e1b921590ff0b87da4d1a68
Smokowski, Kevin (ks6305) <kevin.smokowski@att.com>
2019-09-13 15:00:06 +0000

    create interfaces for sli
    first commit to create interfaces more changes coming
    
    Issue-ID: CCSDK-1723
    Signed-off-by: Smokowski, Kevin (ks6305) <kevin.smokowski@att.com>
    Change-Id: I688b9633010b66ae9386f6c7f5a2e906ca55ac72

---
+++ sli/impl/src/test/java/org/onap/ccsdk/sli/core/sli/provider/base/BadPlugin.java
@@ -0,0 +1,54 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * ONAP : CCSDK
+ * ================================================================================
+ * Copyright (C) 2017 AT&T Intellectual Property. All rights
+ * 						reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.ccsdk.sli.core.sli.provider.base;
+
+import java.util.Map;
+import org.onap.ccsdk.sli.core.api.exceptions.SvcLogicException;
+import org.onap.ccsdk.sli.core.api.extensions.SvcLogicJavaPlugin;
+
+
+public class BadPlugin implements SvcLogicJavaPlugin {
+    public String selectLunch(Map<String, String> parameters, SvcLogicContextImpl ctx) throws SvcLogicException {
+        String day = parameters.get("day");
+        if (day == null || day.length() < 1) {
+            throw new SvcLogicException("What day is it?");
+        }
+        switch (day) {
+        case ("monday"): {
+            return "pizza";
+        }
+        case ("tuesday"): {
+            return "soup";
+        }
+        case ("wednesday"): {
+            return "salad";
+        }
+        case ("thursday"): {
+            return "sushi";
+        }
+        case ("friday"): {
+            return "bbq";
+        }
+        }
+        throw new SvcLogicException("Lunch cannot be served");
+    }
+}
---
+++ sli/impl/src/test/java/org/onap/ccsdk/sli/core/sli/provider/base/SvcLogicContextTest.java
@@ -0,0 +1,59 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * ONAP : CCSDK
+ * ================================================================================
+ * Copyright (C) 2017 AT&T Intellectual Property. All rights
+ * 						reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.ccsdk.sli.core.sli.provider.base;
+
+import java.io.InputStream;
+import java.util.Enumeration;
+import java.util.Properties;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import org.onap.ccsdk.sli.core.sli.provider.base.SvcLogicContextImpl;
+import org.w3c.dom.Document;
+import junit.framework.TestCase;
+
+public class SvcLogicContextTest extends TestCase {
+
+	
+	public void testMerge() {
+		
+		try {
+			InputStream testStr = getClass().getResourceAsStream("/mergetest.xml");
+			DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+			DocumentBuilder db = dbf.newDocumentBuilder();
+
+			Document theDocument = db.parse(testStr);
+			SvcLogicContextImpl ctx = new SvcLogicContextImpl();
+			ctx.mergeDocument("test-merge", theDocument);
+			Properties props = ctx.toProperties();
+			for (Enumeration e = props.propertyNames(); e.hasMoreElements() ; ) {
+				String propName = (String) e.nextElement();
+                System.out.println(propName + " = " + props.getProperty(propName));
+				
+			}
+		} catch (Exception e) {
+            e.printStackTrace();
+			fail("Caught exception trying to merge");
+		}
+		
+	}
+
+}
