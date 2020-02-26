2c78899f04fd40115d3c4cc606e8380c00cdda0b
Smokowski, Kevin (ks6305) <kevin.smokowski@att.com>
2020-02-26 18:42:16 +0000

    update sliapi-springboot
    format source, use provider base and minor changes
    
    Issue-ID: CCSDK-2121
    Signed-off-by: Smokowski, Kevin (ks6305) <kevin.smokowski@att.com>
    Change-Id: I4066ce5e15b1fa298b6e3de6ed8d7d1794b12013

--- sliapi/springboot/README.md
+++ sliapi/springboot/README.md
@@ -1,10 +1,29 @@
 This directory contains a demo springboot implementation of the SLI-API healthcheck method.
 
 To start this server, run:
-
-java -jar -DserviceLogicDirectory=src/main/resources target/sliapi-springboot-{version}.jar
+mvn -DserviceLogicDirectory=src/main/resources spring-boot:run
 
 This will start a servlet on port 8080.  To test to that servlet, post a blank
 message to that port:
 
-curl http://127.0.0.1:8080/SLI-API:healthcheck -X POST -H "Content-Type: application/json"
+curl http://127.0.0.1:8080/restconf/operations/SLI-API:healthcheck -X POST -H "Content-Type: application/json"
+
+Requests can also be sent to `http://localhost:8080/executeGraph`
+
+The graph details need to match a graph which has been loaded and activated
+
+An example request
+```
+{
+    "graphDetails": {
+        "module": "prov",
+        "rpc": "test",
+        "mode": "sync"
+    },
+    "input": {
+        "name": "Hello World",
+        "test": "one",
+        "mixed": "cAsE"
+    }
+}
+```
--- sliapi/springboot/src/main/java/org/onap/ccsdk/sli/core/sliapi/springboot/RestconfApiController.java
+++ sliapi/springboot/src/main/java/org/onap/ccsdk/sli/core/sliapi/springboot/RestconfApiController.java
@@ -1,126 +1,126 @@
 package org.onap.ccsdk.sli.core.sliapi.springboot;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.gson.*;
-import org.onap.ccsdk.sli.core.sli.*;
-import org.onap.ccsdk.sli.core.sli.provider.SvcLogicClassResolver;
-import org.onap.ccsdk.sli.core.sli.provider.SvcLogicPropertiesProviderImpl;
-import org.onap.ccsdk.sli.core.sli.provider.SvcLogicService;
-import org.onap.ccsdk.sli.core.sli.provider.SvcLogicServiceImpl;
-import org.onap.ccsdk.sli.core.sli.provider.base.InMemorySvcLogicStore;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Optional;
+import java.util.Properties;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.onap.ccsdk.sli.core.sli.ConfigurationException;
+import org.onap.ccsdk.sli.core.sli.SvcLogicException;
+import org.onap.ccsdk.sli.core.sli.SvcLogicLoader;
+import org.onap.ccsdk.sli.core.sli.SvcLogicStore;
+import org.onap.ccsdk.sli.core.sli.SvcLogicStoreFactory;
+import org.onap.ccsdk.sli.core.sli.provider.base.HashMapResolver;
 import org.onap.ccsdk.sli.core.sli.provider.base.SvcLogicPropertiesProvider;
 import org.onap.ccsdk.sli.core.sli.provider.base.SvcLogicResolver;
-import org.onap.ccsdk.sli.core.sliapi.model.ExecuteGraphInput;
-import org.onap.ccsdk.sli.core.sliapi.model.HealthcheckInput;
+import org.onap.ccsdk.sli.core.sli.provider.base.SvcLogicServiceBase;
+import org.onap.ccsdk.sli.core.sli.provider.base.SvcLogicServiceImplBase;
 import org.onap.ccsdk.sli.core.sliapi.model.ResponseFields;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.SpringApplication;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import javax.servlet.http.HttpServletRequest;
-import javax.validation.Valid;
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Optional;
-import java.util.Properties;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @javax.annotation.Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2020-02-20T12:50:11.207-05:00")
 
 @Controller
 public class RestconfApiController implements RestconfApi {
 
-    private final ObjectMapper objectMapper;
+	private final ObjectMapper objectMapper;
+	private final HttpServletRequest request;
+	private static SvcLogicServiceBase svc;
+	private static final Logger log = LoggerFactory.getLogger(RestconfApiController.class);
 
-    private final HttpServletRequest request;
+	@org.springframework.beans.factory.annotation.Autowired
+	public RestconfApiController(ObjectMapper objectMapper, HttpServletRequest request) {
+		this.objectMapper = objectMapper;
+		this.request = request;
 
-    private static SvcLogicService svc;
-    Logger log = LoggerFactory.getLogger(RestconfApiController.class);
+		SvcLogicPropertiesProvider propProvider = new SvcLogicPropertiesProvider() {
 
-    @org.springframework.beans.factory.annotation.Autowired
-    public RestconfApiController(ObjectMapper objectMapper, HttpServletRequest request) {
-        this.objectMapper = objectMapper;
-        this.request = request;
+			@Override
+			public Properties getProperties() {
+				Properties props = new Properties();
+				String propPath = "src/main/resources/svclogic.properties";
+				System.out.println(propPath);
+				try (FileInputStream fileInputStream = new FileInputStream(propPath)) {
+					props = new Properties();
+					props.load(fileInputStream);
+				} catch (final IOException e) {
+					log.error("Failed to load properties for file: {}", propPath,
+							new ConfigurationException("Failed to load properties for file: " + propPath, e));
+				}
+				return props;
+			}
+		};
 
-        SvcLogicPropertiesProvider propProvider = new SvcLogicPropertiesProviderImpl();
-        SvcLogicStore store = null;
-        try {
-            store = SvcLogicStoreFactory.getSvcLogicStore(propProvider.getProperties());
-        } catch (SvcLogicException e) {
-            log.error("Cannot create SvcLogicStore", e);
-            return;
-        }
+		SvcLogicStore store = null;
+		try {
+			store = SvcLogicStoreFactory.getSvcLogicStore(propProvider.getProperties());
+		} catch (SvcLogicException e) {
+			log.error("Cannot create SvcLogicStore", e);
+			return;
+		}
 
-        String serviceLogicDirectory = System.getProperty("serviceLogicDirectory");
-        System.out.println("serviceLogicDirectory is "+serviceLogicDirectory);
-        SvcLogicLoader loader = new SvcLogicLoader(serviceLogicDirectory, store);
+		String serviceLogicDirectory = System.getProperty("serviceLogicDirectory");
+		System.out.println("serviceLogicDirectory is " + serviceLogicDirectory);
+		SvcLogicLoader loader = new SvcLogicLoader(serviceLogicDirectory, store);
 
-        try {
-            loader.loadAndActivate();
-        } catch (IOException e) {
-            log.error("Cannot load directed graphs", e);
-        }
-        SvcLogicResolver resolver = new SvcLogicClassResolver();
+		try {
+			loader.loadAndActivate();
+		} catch (IOException e) {
+			log.error("Cannot load directed graphs", e);
+		}
+		SvcLogicResolver resolver = new HashMapResolver();
 
+		svc = new SvcLogicServiceImplBase(store, resolver);
 
-        try {
-            svc = new SvcLogicServiceImpl(new SvcLogicPropertiesProviderImpl(), resolver);
-        } catch (SvcLogicException e) {
-            log.error("Cannot execute directed graph", e);
-        }
-    }
+	}
 
-    @Override
-    public ResponseEntity<ResponseFields> healthcheck() {
-        SvcLogicContext ctx = new SvcLogicContext();
-        ResponseFields resp = new ResponseFields();
+	@Override
+	public ResponseEntity<ResponseFields> healthcheck() {
+		ResponseFields resp = new ResponseFields();
 
+		try {
+			log.info("Calling SLI-API:healthcheck DG");
+			Properties inputProps = new Properties();
+			Properties respProps = svc.execute("sli", "healthcheck", null, "sync", inputProps);
+			if (respProps == null) {
+				log.info("DG execution returned no properties!");
+			} else {
+				log.info("DG execution returned properties");
+				for (String key : respProps.stringPropertyNames()) {
+					log.info("DG returned property " + key + " = " + respProps.getProperty(key));
+				}
+			}
+			resp.setAckFinalIndicator(respProps.getProperty("ack-final-indicator", "Y"));
+			resp.setResponseCode(respProps.getProperty("error-code", "200"));
+			resp.setResponseMessage(respProps.getProperty("error-message", "Success"));
 
-        try {
+			return (new ResponseEntity<>(resp, HttpStatus.OK));
+		} catch (Exception e) {
+			resp.setAckFinalIndicator("true");
+			resp.setResponseCode("500");
+			resp.setResponseMessage(e.getMessage());
+			log.error("Error calling healthcheck directed graph", e);
 
-            log.info("Calling SLI-API:healthcheck DG");
-            Properties inputProps = new Properties();
-            Properties respProps = svc.execute("sli", "healthcheck" , null, "sync", inputProps);
-            if (respProps == null) {
-                log.info("DG execution returned no properties!");
-            } else {
-                log.info("DG execution returned properties");
-                for (String key: respProps.stringPropertyNames()) {
-                    log.info("DG returned property "+key+" = "+respProps.getProperty(key));
-                }
-            }
-            resp.setAckFinalIndicator(respProps.getProperty("ack-final-indicator", "Y"));
-            resp.setResponseCode(respProps.getProperty("error-code", "200"));
-            resp.setResponseMessage(respProps.getProperty("error-message", "Success"));
+		}
+		return (new ResponseEntity<>(resp, HttpStatus.INTERNAL_SERVER_ERROR));
+	}
 
-            return(new ResponseEntity<>(resp, HttpStatus.OK));
-        }
-        catch (Exception e) {
-            resp.setAckFinalIndicator("true");
-            resp.setResponseCode("500");
-            resp.setResponseMessage(e.getMessage());
-            log.error("Error calling healthcheck directed graph", e);
+	@Override
+	public Optional<ObjectMapper> getObjectMapper() {
+		return Optional.ofNullable(objectMapper);
+	}
 
-        }
-
-        return(new ResponseEntity<>(resp, HttpStatus.INTERNAL_SERVER_ERROR));
-
-    }
-
-    @Override
-    public Optional<ObjectMapper> getObjectMapper() {
-        return Optional.ofNullable(objectMapper);
-    }
-
-    @Override
-    public Optional<HttpServletRequest> getRequest() {
-        return Optional.ofNullable(request);
-    }
-
-
-
-
+	@Override
+	public Optional<HttpServletRequest> getRequest() {
+		return Optional.ofNullable(request);
+	}
 
 }
