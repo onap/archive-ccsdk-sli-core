855974618f2783aff89c36d7fdd97450667630cc
Dan Timoney <dtimoney@att.com>
2020-10-21 16:16:06 -0400

    Fix pattern validation
    Pattern validation incorrectly disallows period as part of file
    name
    
    Change-Id: I3b3692093e16c41c312b326246a5dee83f674ee0
    Issue-ID: CCSDK-2918
    Signed-off-by: Dan Timoney <dtimoney@att.com>

--- utils/provider/src/main/java/org/onap/ccsdk/sli/core/utils/PathValidator.java
+++ utils/provider/src/main/java/org/onap/ccsdk/sli/core/utils/PathValidator.java
@@ -3,15 +3,15 @@ import java.util.regex.Pattern;
 
 public class PathValidator {
     public static boolean isValidXmlPath(String path) {
-        Pattern allowList = Pattern.compile("[-\\w/\\/]+\\.xml$");
+        Pattern allowList = Pattern.compile("[-.\\w/\\/]+\\.xml$");
         return (allowList.matcher(path).matches());
     }
     public static boolean isValidPropertiesPath(String path) {
-        Pattern allowList = Pattern.compile("[-\\w/\\/]+\\.properties$");
+        Pattern allowList = Pattern.compile("[-.\\w/\\/]+\\.properties$");
         return (allowList.matcher(path).matches());
     }
     public static boolean isValidFilePath(String path) {
-        Pattern allowList = Pattern.compile("[-\\w/\\/]+");
+        Pattern allowList = Pattern.compile("[-.\\w/\\/]+$");
         return (allowList.matcher(path).matches());
     }
 }
